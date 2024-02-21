7e51805dcf2dafd3243d7a841dd7dd8207a31715
tech-hunter <kingkushwaha93@gmail.com>
2024-02-21 19:42:09 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,40 +1,42 @@
-const hre = require("hardhat")
-const { items } = require("../src/items.json")
+  const hre = require("hardhat")
+  const { items } = require("../src/items.json")
+  const { ethers } = require("ethers");
 
-const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
-}
-
-async function main() {
-  // Setup accounts
-  const [deployer] = await ethers.getSigners()
-
-  // deploy
-  const Medicine = await hre.ethers.getContractFactory("Medicine")
-  const medicine = await Medicine.deploy()
-  await medicine.deployed()
-
-  console.log(`Deployed Contract at: ${medicine.address}\n`)
-
-  //list items
-  for (let i = 0; i < items.length; i++) {
-    const transaction = await medicine.connect(deployer).list(
-      items[i].id,
-      items[i].name,
-      items[i].category,
-      items[i].image,
-      tokens(items[i].price),
-      items[i].rating,
-      items[i].stock,
-    )
-
-    await transaction.wait()
-
-    console.log(`Listed item ${items[i].id}: ${items[i].name}`)
+  const tokens = (n) => {
+    return ethers.utils.parseUnits(n.toString(), 'ether')
   }
-}
 
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+  async function main() {
+    // Setup accounts
+    // const [deployer] = await ethers.getSigners()
+    const [deployer] = await hre.ethers.getSigners();
+
+    // deploy
+    const Medicine = await hre.ethers.getContractFactory("Medicine")
+    const medicine = await Medicine.deploy()
+    await medicine.deployed()
+
+    console.log(`Deployed Contract at: ${medicine.address}\n`)
+
+    //list items
+    for (let i = 0; i < items.length; i++) {
+      const transaction = await medicine.connect(deployer).list(
+        items[i].id,
+        items[i].name,
+        items[i].category,
+        items[i].image,
+        tokens(items[i].price),
+        items[i].rating,
+        items[i].stock,
+      )
+
+      await transaction.wait()
+
+      console.log(`Listed item ${items[i].id}: ${items[i].name}`)
+    }
+  }
+
+  main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
